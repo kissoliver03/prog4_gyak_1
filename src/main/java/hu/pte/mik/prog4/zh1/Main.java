91cba7b11eb4f8b9668946a819fbb9885d25399a
kissoliver03 <kissoliver03@gmail.com>
2026-04-03 11:16:39 +0200

    git init

---
+++ src/main/java/hu/pte/mik/prog4/zh1/Main.java
@@ -0,0 +1,7 @@
+package hu.pte.mik.prog4.zh1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+}

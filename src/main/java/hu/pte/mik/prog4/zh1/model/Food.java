d01eca2671231110a4c43030e3b87c5cf09c185d
kissoliver03 <kissoliver03@gmail.com>
2026-04-03 11:23:11 +0200

    feat: added Food class and generated all the getters and setters, hash code, tostring and equals methods

---
+++ src/main/java/hu/pte/mik/prog4/zh1/model/Food.java
@@ -0,0 +1,75 @@
+package hu.pte.mik.prog4.zh1.model;
+
+import java.util.Objects;
+
+public class Food {
+    Long id;
+    String restaurantName;
+    String foodName;
+    String price;
+
+    public Food() {
+    }
+
+    public Food(Long id, String restaurantName, String foodName, String price) {
+        this.id = id;
+        this.restaurantName = restaurantName;
+        this.foodName = foodName;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "id=" + id +
+                ", restaurantName='" + restaurantName + '\'' +
+                ", foodName='" + foodName + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Food food = (Food) o;
+        return Objects.equals(id, food.id) && Objects.equals(restaurantName, food.restaurantName) && Objects.equals(foodName, food.foodName) && Objects.equals(price, food.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, restaurantName, foodName, price);
+    }
+}
